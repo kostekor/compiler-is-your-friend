871193e7b8f9819df5e0f9d1b1cb1a545772c369
Mongkolchai Rungrueang <mongkolchai.r@codium.co>
2020-08-05 13:41:04 +0700

    [answer] Question 1

--- 1.correctness.ts
+++ 1.correctness.ts
@@ -12,3 +12,21 @@ Questions:
 1) How to ensure that the input is a string at compile time?
 2) What did you learn from this exercise?
 */
+
+const nType = 1;
+const strType = 'sscscrscriscript';
+
+function scriptAt(str: string) {
+    return str.search('Script');
+}
+
+console.log('the answer: ', scriptAt(strType));
+
+/** error this line */
+console.log('the answer: ', scriptAt(input1));
+
+/**
+ * Answer
+ * 1. ensure because parameter is assigned type with string
+ * 2. type force us to assign a function input correctly. We can ensure that function get input correctly.
+ */
